d9860abc044e9dfddfce6ff304b930a78e6ed7c3
Shawal Ali <shawalnetwork@gmail.com>
2023-04-12 11:32:40 +0500

    header completed

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1 className='headerHeading'>
+        Get It Together And Manage Projects The Right Way.
+      </h1>
+      <p className='headerText'>
+        udix is the world's first smart workplace. We bring all your team's
+        content together while letting you use the tools you love.
+      </p>
+      <div className='subscribe'>
+        <input className='subscribeInput' placeholder='Email' />
+        <button className='btnSubcribe'>Subscribe</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from '../components/header/Header';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
