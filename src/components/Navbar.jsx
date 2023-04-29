5c073ac05b7a5cf83eacb3e34b7f44aa517599fa
Shawal Ali <shawalnetwork@gmail.com>
2023-04-30 02:01:18 +0500

    new changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import logo from '../assets/logo.svg';
+
+const Navbar = () => {
+  return (
+    <div className='primary-header'>
+      <div className='container'>
+        <div className='top-left'>
+          <img src={logo} alt='' />
+          <ul className='top-ul text-secondary fw-medium uppercase'>
+            <li className='top-li'>
+              <a className='link' href='#'>
+                Home
+              </a>
+            </li>
+            <li className='top-li'>
+              <a className='link' href='#'>
+                Blog
+              </a>
+            </li>
+            <li className='top-li'>
+              <a className='link' href='#'>
+                Product
+              </a>
+            </li>
+            <li className='top-li'>
+              <a className='link' href='#'>
+                Contacts
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className='top-right'>
+          <button className='button uppercase'>Sign in</button>
+          <button className='button uppercase outline'>Sign up</button>
+        </div>
+        <div className='top-right-mobile'>
+          <button className='button uppercase outline'>Menu</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
